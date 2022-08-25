34ff2d6eeabab7e2da7db54ed9fdd483f4a283f3
Jean Charles Piette <piette.jc@me.com>
2022-08-25 11:06:01 +0100

    add addresses table

---
+++ db/migrate/20220825100450_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :addresses do |t|
+      t.string :street
+      t.string :zip_code
+      t.string :city
+      t.string :country
+      t.boolean :is_default
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_182258) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_100450) do
+  create_table "addresses", force: :cascade do |t|
+    t.string "street"
+    t.string "zip_code"
+    t.string "city"
+    t.string "country"
+    t.boolean "is_default"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
@@ -26,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_182258) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
 end
