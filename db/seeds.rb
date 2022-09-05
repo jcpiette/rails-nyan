4ba8f94d48e95985efa7772df64bd9871c9d6557
Paulo Gomes <pjgomes85@hotmail.com>
2022-09-05 12:48:15 +0100

    user seeds completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!(first_name: "Paulo", last_name: "Gomes", email: "pjgomes85@hotmail.com", password: "Test123")
+User.create!(first_name: "Theo", last_name: "P.", email: "kmstheodore@gmail.com", password: "Test123")
+User.create!(first_name: "Olivia", last_name: "S.", email: "oliviapatterson123@live.com", password: "Test123")
+User.create!(first_name: "jay", last_name: "P.", email: "piette.jc@me.com", password: "Test123")
