11ec1857bed75272d40988542a3c73149340a8fb
Paulo Gomes <pjgomes85@hotmail.com>
2022-09-05 15:49:07 +0100

    address working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,24 @@ class PagesController < ApplicationController
   end
 
   def address
+    @address = User.find(params[:address])
+    @address.save
+    # Find your user by current user
+    # Assign the address hash to the address attribute of said user
+    # Save it
+    # if params['address']
   end
 
   def preference
+
   end
 
   def notification
+  end
+
+  private
+
+  def address_params
+    params.require(:planet).permit(:name, :price, :desc, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :user_friends, :events, only: [:index, :show, :new, :edit, :destroy]
-    get 'users/notification', to: 'pages#notification'
-    get 'users/address', to: 'pages#address'
-    get 'users/preferences', to: "pages#preferences"
-   
+    get '/notification', to: 'pages#notification'
+    get '/address', to: 'pages#address'
+    get '/preferences', to: "pages#preferences"
+
   end
   # Defines the root path route ("/")
   # root "articles#index"
