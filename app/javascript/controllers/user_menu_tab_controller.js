3725602aaae59b0d4a3431de32829159427b5014
Paulo Gomes <pjgomes85@hotmail.com>
2022-09-06 12:41:51 +0100

    seetings page 80% complete

--- app/javascript/controllers/user_menu_tab_controller.js
+++ app/javascript/controllers/user_menu_tab_controller.js
@@ -2,22 +2,53 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="user-menu-tab"
 export default class extends Controller {
+  static targets = ["userInfo", "address","preference","notification", "userInfoTab"]
+
   connect() {
+    console.log("connected to user menu");
+    console.log(this.userInfoTarget);
   }
 
-  displayAccount() {
-    alert("account");
+  displayAccount(e) {
+    e.preventDefault();
+    console.log(this.addressTarget);
+    this.addressTarget.style.display = "block";
+    this.userInfoTarget.style.display = "none";
+    this.preferenceTarget.style.display = "none";
+    this.notificationTarget.style.display = "none";
+    this.targets.classList.remove('tab-selected');
+    e.currentTarget.classList.toggle('tab-selected');
   }
 
-  displayAddress() {
-    alert("address");
+  displayAddress(e) {
+    e.preventDefault();
+    console.log(this.addressTarget);
+    this.addressTarget.style.display = "block";
+    this.userInfoTarget.style.display = "none";
+    this.preferenceTarget.style.display = "none";
+    this.notificationTarget.style.display = "none";
+    this.targets.classList.remove('tab-selected');
+    e.currentTarget.classList.toggle('tab-selected');
   }
 
-  displayPreference() {
-    alert("prefe");
+  displayPreference(e) {
+    console.log(this.preferenceTarget);
+    this.preferenceTarget.style.display = "block";
+    this.userInfoTarget.style.display = "none";
+    this.addressTarget.style.display = "none";
+    this.notificationTarget.style.display = "none";
+    this.targets.classList.remove('tab-selected');
+    e.currentTarget.classList.toggle('tab-selected');
   }
 
-  displayNotification() {
-    alert("notif");
+  displayNotification(e) {
+    e.preventDefault();
+    console.log(this.notificationTarget);
+    this.notificationTarget.style.display = "block";
+    this.userInfoTarget.style.display = "none";
+    this.addressTarget.style.display = "none";
+    this.preferenceTarget.style.display = "none";
+    this.targets.classList.remove('tab-selected');
+    e.currentTarget.classList.toggle('tab-selected');
   }
 }
