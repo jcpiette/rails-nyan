f6801065dce7b250404710a6e80acc9eb54ef299
Jean Charles Piette <piette.jc@me.com>
2022-09-06 10:49:40 +0100

    fix seed issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 #Clean Database
-puts "Cleaning..."
+puts 'Cleaning...'
 
 User.destroy_all
 
-puts "Database cleaned!"
+puts 'Database cleaned!'
 
 #Create Users
-puts "Creating users..."
+puts 'Creating users...'
 
 User.create!(first_name: "Paulo", last_name: "Gomes", email: "pjgomes85@hotmail.com", password: "Test123")
 User.create!(first_name: "Theo", last_name: "P.", email: "kmstheodore@gmail.com", password: "Test123")
 User.create!(first_name: "Olivia", last_name: "S.", email: "oliviapatterson123@live.com", password: "Test123")
 User.create!(first_name: "jay", last_name: "P.", email: "piette.jc@me.com", password: "Test123")
 
-puts "Users created!"
+puts 'Users created!'
