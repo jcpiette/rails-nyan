9c97a1ae5c7815697f5fb58d2a608c6564913493
Olivia Patterson <oliviapatterson123@live.com>
2022-09-07 14:43:43 +0100

    commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,6 +22,14 @@ class EventsController < ApplicationController
   def edit
   end
 
+  def user_suggestions
+    location = [ User.find(session[:user_id].latitude, User.find(session[:user_id]).longitude)]
+    type = User.find(session[:user_id]).preference_type
+    minprice, maxprice = [User.find(session[:user_id]).preference_budget, User.find(session[:user_id]).preference_budget]
+    radius = '1000'
+
+  end
+
   def suggestions
     location = find_event_location()
     type = 'restaurant'
@@ -44,7 +52,7 @@ class EventsController < ApplicationController
       place_ids << place['place_id']
     end
     @suggestions = []  #ALL PLACE ID
-    place_photo= []   #GET THE ID OF THE PHOTO
+    place_photo = []   #GET THE ID OF THE PHOTO
     place_ids.each_with_index do |place_id, index|
       url = URI("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{place_id}&fields=name%2Cformatted_address%2Cgeometry%2Cprice_level%2Cadr_address%2Crating%2Ctypes%2Cphotos&key=AIzaSyCSlUELYAxe0sfUJpUEJQU3TcF1OXNS-xs")
       https = Net::HTTP.new(url.host, url.port)
@@ -65,41 +73,7 @@ class EventsController < ApplicationController
         'rating' => json_file['result']['rating']
       }
     end
-    return @suggestions
-  end
-
-  def make_json(url)
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
-    request = Net::HTTP::Get.new(url)
-    response = https.request(request)
-    file = response.read_body
-    json_file = JSON.parse(file)
-    return json_file
-  end
-
-  def find_places(location, radius, type, min_budget, max_budget)
-    id_array = []
-    url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{location}&radius=#{radius}&keyword=#{type}&minprice=#{min_budget}maxprice=#{max_budget}&key=AIzaSyCSlUELYAxe0sfUJpUEJQU3TcF1OXNS-xs")
-    json_file = make_json(url)
-    json_file['results'].each do |place|
-      id_array << place['place_id']
-    end
-    id_array
-  end
-
-  def get_info(id)
-    url = URI("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{id}&fields=name%2Cformatted_address%2Cgeometry%2Cprice_level%2Cadr_address%2Crating%2Ctypes&key=AIzaSyCSlUELYAxe0sfUJpUEJQU3TcF1OXNS-xs")
-    puts url
-    json_file = make_json(url)
-    place_hash = {
-      'name' => json_file['result']['name'],
-      #'address' => json_file['result']['formatted_address'],
-      #'adr_address' => json_file['result']['adr_address'],
-      'price level' => json_file['result']['price_level'],
-      'rating' => json_file['result']['rating'],
-    }
-    place_hash
+    @suggestions
   end
 
   def find_event_type
