59e1b4a83f78f3810c38cc7ad17f6dddf30c040b
Olivia Patterson <oliviapatterson123@live.com>
2022-09-08 15:03:32 +0100

    js issue fix

--- db/schema.rb
+++ db/schema.rb
@@ -116,7 +116,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_193308) do
     t.datetime "updated_at", null: false
     t.string "provider", limit: 50, default: "", null: false
     t.string "uid", limit: 500, default: "", null: false
-    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
