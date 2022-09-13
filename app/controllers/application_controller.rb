81e7a962ddd8b11aba17f9c6fd6c09e266ffe7a9
Olivia Patterson <oliviapatterson123@live.com>
2022-09-13 12:01:37 +0100

    fixed notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_notifications
+  before_action :set_notifications, unless: :not_logged_in?
 
+  def not_logged_in?
+    current_user == nil
+  end
   protected
+
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
