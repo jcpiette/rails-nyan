832032f8f29b8fd4ff6aa3203f99b86b11e4d246
Olivia Patterson <oliviapatterson123@live.com>
2022-09-15 13:51:17 +0100

    seeds changed for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,16 @@ puts 'Database cleaned!'
 # Create Users
 puts 'Creating users...'
 # us
-user1 = User.create!(first_name: 'Paulo', last_name: 'Gomes', full_name: 'Paulo Gomes', email: "pjgomes85@hotmail.com", password: "Test123", latitude: '51.498356', longitude: '-0.176894', preference_type: 'restaurant', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058301/xclthgkn9itvuyk8szg7.jpg") #imperial college
-user2 = User.create!(first_name: 'Theo', last_name: 'Simpson', full_name: 'Theo Simpson', email: "kmstheodore@gmail.com", password: "Test123", latitude: '51.541257', longitude: '-0.153317', preference_type: 'restaurant', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058287/tqque2jr3raeo50hpq0h.jpg")
-user3 = User.create!(first_name: 'Olivia', last_name: 'Patterson', full_name: 'Olivia Patterson', email: "oliviapatterson123@live.com", password: "Test123", latitude: '51.5105', longitude: '-0.1872', preference_type: 'restaurant', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058273/udr7otyyxhf72iclmatn.jpg") #queensway
-user4 = User.create!(first_name: 'Jay', last_name: 'P.', full_name: 'Jay P.', email: "piette.jc@me.com", password: "Test123", latitude: '51.5556', longitude: '0.1762', preference_type: 'restaurant', preference_budget: '4', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058293/jjbumwkq1nh14xl7c7pr.jpg") #hampstead
+user1 = User.create!(first_name: 'Paulo', last_name: 'Gomes', full_name: 'Paulo Gomes', email: "pjgomes85@hotmail.com", password: "Test123", latitude: '51.498356', longitude: '-0.176894', preference_type: 'bar', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058301/xclthgkn9itvuyk8szg7.jpg") #imperial college
+user2 = User.create!(first_name: 'Theo', last_name: 'Simpson', full_name: 'Theo Simpson', email: "kmstheodore@gmail.com", password: "Test123", latitude: '51.541257', longitude: '-0.153317', preference_type: 'bar', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058287/tqque2jr3raeo50hpq0h.jpg")
+user3 = User.create!(first_name: 'Olivia', last_name: 'Patterson', full_name: 'Olivia Patterson', email: "oliviapatterson123@live.com", password: "Test123", latitude: '51.519479', longitude: '-0.149900', preference_type: 'restaurant', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058273/udr7otyyxhf72iclmatn.jpg") #queensway
+user4 = User.create!(first_name: 'Jay', last_name: 'P.', full_name: 'Jay P.', email: "piette.jc@me.com", password: "Test123", latitude: '51.5556', longitude: '0.1762', preference_type: 'bar', preference_budget: '4', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058293/jjbumwkq1nh14xl7c7pr.jpg") #hampstead
 # randoms
-user5 = User.create!(first_name: 'Harry', last_name: 'Potter', full_name: 'Harry Potter', email: "ckent@gmail.com", password: "Test123", latitude: '51.5445', longitude: '0.1522', preference_type: 'restaurant', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058350/pyxd5dy3hbskqytojxxc.jpg") #chalk farm
-user6 = User.create!(first_name: 'Draco', last_name: 'Malfoy', full_name: 'Draco Malfoy', email: "gbarlow@gmail.com", password: "Test123", latitude: '51.50360210286566', longitude: '-0.15148749007201978', preference_type: 'restaurant', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058358/i5l2n68v2ypwpoyhlrpd.jpg")
-user7 = User.create!(first_name: 'Tom', last_name: 'Riddle', full_name: 'Tom Riddle', email: "jsmith@gmail.com", password: "Test123", latitude: '51.50360210286566', longitude: '-0.15148749007201978', preference_type: 'restaurant', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058319/wunoihihpm4gykc3mzzw.png")
-user8 = User.create!(first_name: 'Ginny', last_name: 'Weasley', full_name: 'Ginny Weasley', email: "sknowles@gmail.com", password: "Test123", latitude: '51.50360210286566', longitude: '-0.15148749007201978', preference_type: 'restaurant', preference_budget: '4', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058336/zedxch4wlyohxzk49btj.jpg")
-user9 = User.create!(first_name: 'Hermione', last_name: 'Granger', full_name: 'Hermione Granger', email: "adobbs@gmail.com", password: "Test123", latitude: '51.50360210286566', longitude: '-0.15148749007201978', preference_type: 'restaurant', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058343/aun8xsd66iotnk9vjqvl.jpg")
+user5 = User.create!(first_name: 'Harry', last_name: 'Potter', full_name: 'Harry Potter', email: "ckent@gmail.com", password: "Test123", latitude: '51.5445', longitude: '0.1522', preference_type: 'bar', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058350/pyxd5dy3hbskqytojxxc.jpg") #chalk farm
+user6 = User.create!(first_name: 'Draco', last_name: 'Malfoy', full_name: 'Draco Malfoy', email: "gbarlow@gmail.com", password: "Test123", latitude: '51.511184', longitude: '-0.149958', preference_type: 'bar', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058358/i5l2n68v2ypwpoyhlrpd.jpg")
+user7 = User.create!(first_name: 'Tom', last_name: 'Riddle', full_name: 'Tom Riddle', email: "jsmith@gmail.com", password: "Test123", latitude: '51.515271', longitude: '-0.156584', preference_type: 'bar', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058319/wunoihihpm4gykc3mzzw.png")
+user8 = User.create!(first_name: 'Ginny', last_name: 'Weasley', full_name: 'Ginny Weasley', email: "sknowles@gmail.com", password: "Test123", latitude: '51.50360210286566', longitude: '-0.15148749007201978', preference_type: 'bar', preference_budget: '4', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058336/zedxch4wlyohxzk49btj.jpg")
+user9 = User.create!(first_name: 'Hermione', last_name: 'Granger', full_name: 'Hermione Granger', email: "adobbs@gmail.com", password: "Test123", latitude: '51.50360210286566', longitude: '-0.15148749007201978', preference_type: 'bar', preference_budget: '3', img_url:"https://res.cloudinary.com/do31e5zv5/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:0C2345/v1663058343/aun8xsd66iotnk9vjqvl.jpg")
 
 # file = URI.open("https://res.cloudinary.com/dkywz955g/image/upload/v1661504913/cld-sample.jpg")
 # user1.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
@@ -94,28 +94,17 @@ event1 = Event.create!(
   description: 'Meeting up for lunch with the gang.',
   user_id: user3.id,
   event_date: Date.today + 10,
-  location: "Hoxton"
+  location: '138 Kingsland Road'
 )
 EventMember.create!(user_id: user1.id, event_id: event1.id)
 EventMember.create!(user_id: user2.id, event_id: event1.id)
-EventMember.create!(user_id: user4.id, event_id: event1.id)
-
-# event2 = Event.create!(
-#   title: 'Family BBQ',
-#   description: 'Come along for some home cooked dinner.',
-#   user_id: user3.id,
-#   event_date: Date.today + 20
-# )
-# EventMember.create(user_id: user8.id, event_id: event2.id)
-# EventMember.create(user_id: user7.id, event_id: event2.id)
-
 
 event3 = Event.create!(
   title: 'Pottery Class',
   description: 'Join me for a fun and creative pottery class.',
   user_id: user3.id,
-  event_date: Date.today + 30
+  event_date: Date.today + 20
 )
-EventMember.create(user_id: user9.id, event_id: event3.id)
+EventMember.create!(user_id: user4.id, event_id: event3.id)
 
 puts 'Events created!'
